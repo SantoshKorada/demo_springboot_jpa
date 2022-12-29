d79fe7df46c6fa61a85ae0ab873239aa78fe4d2f
koradas <santosh.korada@autodesk.com>
2022-12-29 08:23:52 +0530

    Intitial commit for JPA

---
+++ src/main/java/com/training/demo/DemoSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.training.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootJpaApplication.class, args);
+	}
+
+}
